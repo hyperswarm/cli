4ad19bfcda1a90569766887719432706df214ca6
Mathias Buus <mathiasbuus@gmail.com>
2020-04-20 16:35:37 +0200

    tweak output and options

--- dht.js
+++ dht.js
@@ -6,12 +6,11 @@ const os = require('os')
 const path = require('path')
 const fs = require('fs')
 const dhtrpc = require('dht-rpc')
-const util = require('util')
 
 const argv = minimist(process.argv, {
   boolean: [
-    'ephemeral',
-    'quiet',
+    'adaptive',
+    'verbose',
     'help',
     'json'
   ],
@@ -21,14 +20,15 @@ const argv = minimist(process.argv, {
     'id'
   ],
   default: {
-    ephemeral: true,
-    'id-file': 'dht-rpc-id',
-    quiet: false
+    adaptive: true,
+    'id-file': path.join(os.tmpdir(), 'dht-rpc-id'),
+    verbose: false,
+    port: 49737,
+    address: '0.0.0.0'
   },
   alias: {
-    ephemeral: 'e',
     bootstrap: 'b',
-    quiet: 'q',
+    verbose: 'V',
     address: 'a',
     port: 'p',
     'id-file': 'f',
@@ -40,111 +40,99 @@ const argv = minimist(process.argv, {
 
 if (argv.help) {
   console.error(`Usage: ${process.argv[1]} [options]
+
   --id, -i           [key]   ID for this dht node
   --id-file, -f      [path]  Path to store a random id for this node (ignored if id is given)
   --port, -p         [port]  Specify port to listen to (optional, as dht-nodes don't require listening)
   --address, -a      [addr]  Specify address to listen to (optional, only used if port is given)
   --json                     Output all messages as json.
-  --quiet, -q                Hide announce output
-  --no-ephemeral             Host other peoples keys/values
+  --verbose, -V              Print all lookups,announces,unannounces
   --bootstrap, -b            Specify bootstrap peers (optional)
+  --no-adaptive              Disable adaptive ephemerality
 `)
   process.exit(1)
 }
 
-const id = resolveId(argv.id, argv['id-file'])
-const ephemeral = argv.ephemeral
-const port = argv.port || process.env.PORT
+const idFile = argv['id-file']
+const id = resolveId(argv.id, idFile)
+const adaptive = argv.adaptive
+const port = argv.port
+const address = argv.address
 const bootstrap = argv.boostrap ? [].concat(argv.bootstrap || []) : undefined
-let exitCode = 0
-const dht = createDht({ ephemeral, adaptive: ephemeral, id: id.id, bootstrap })
+const version = require('@hyperswarm/dht/package.json').version
+const verbose = argv.verbose
 
-log(
-  ({ start }) => `Starting Node:
-${util.inspect(start, { colors: true })}
-`,
-  {
-    start: {
-      nodejsVersion: process.version,
-      cliVersion: require('./package.json').version,
-      dhtVersion: require('@hyperswarm/dht/package.json').version,
-      node: {
-        ...id,
-        id: dht.id.toString('hex')
-      },
-      port: port || undefined,
-      address: argv.address || undefined,
-      bootstrap,
-      ephemeral,
-      quiet: argv.quiet
-    }
-  }
-)
+const dht = createDht({ adaptive: adaptive, ephemeral: adaptive, id, bootstrap })
 
-dht.on('listening', function () {
-  log(({ listening: { address, port, family } }) => `Listening to ${address}:${port} (udp,${family})`, { listening: this.socket.address() })
-})
-dht.on('ready', function () {
-  log(() => 'DHT node fully bootstrapped.', { state: 'bootstrapped' })
-})
-dht.on('persistent', function () {
-  log(() => 'DHT is now persistent', { state: 'persitent' })
-})
-dht.on('warning', function (warning) {
-  log(() => `Warning: ${warning.message}`, { warning })
-})
-dht.on('error', function (error) {
-  log(() => `Error: ${error.message}`, { error })
-  exitCode = 1
-  close()
-})
-dht.on('close', function () {
-  log(() => 'DHT node fully closed.', { state: 'closed' })
-  process.exit(exitCode)
-})
-if (port) {
-  dht.listen(port, argv.address)
+const msg = `DHT version ${version}
+
+  id=${id.toString('hex')}
+  id-file=${idFile}
+  port=${port}
+  address=${address}
+  adaptive=${adaptive}
+  bootstrap=${bootstrap || '(default)'}
+  verbose=${verbose}
+`
+
+log(msg, { version, id: id.toString('hex'), idFile, port, address, adaptive, bootstrap, verbose })
+
+if (adaptive) {
+  if (!argv.json) log('Running in adaptive mode. Will go persistent once running for ~30 min and holepunchable')
 }
 
-if (!argv.quiet) {
+dht.on('listening', function () {
+  const { address, port, family } = this.socket.address()
+  log(`Listening on ${address}:${port} (udp,${family})`, { state: 'listening', address, port, family })
+})
+
+dht.on('ready', function () {
+  log('DHT node fully bootstrapped', { state: 'bootstrapped' })
+})
+
+dht.on('initial-nodes', function () {
+  const holepunchable = dht.holepunchable()
+  const remoteAddress = dht.remoteAddress()
+  log(holepunchable ? `Network appears holepunchable (remote address is ${remoteAddress.host}:${remoteAddress.port})` : 'Warning: Network does not appear holepunchable', { holepunchable, remoteAddress })
+})
+
+dht.on('persistent', function () {
+  log('DHT appears holepunchable and stable. Now persistent', { state: 'persistent' })
+})
+
+dht.on('warning', function (warning) {
+  log(`Warning: ${warning.message}`, { warning: { message: warning.message, stack: warning.stack } })
+})
+
+dht.on('error', function (error) {
+  log(`Error: ${error.stack}`, { error: { message: error.message, stack: error.stack } })
+  process.exit(1)
+})
+
+dht.listen(port, address)
+
+if (argv.verbose) {
   for (const event of ['announce', 'unannounce', 'lookup']) {
     dht.on(event, function (target, peer) {
-      log(({ target, peer }) => `Received ${event}:\n${util.inspect({ target: target.toString('hex'), peer }, { colors: true })}`, { event, target, peer })
+      log(`Received ${event}: ${peer.host}:${peer.port} @ ${target.toString('hex')}`, { event, target: target.toString('hex'), peer })
     })
   }
 }
 
-process.on('SIGINT', close)
-
-function close () {
-  log(() => '\nClosing DHT Node.', { state: 'closing' })
-  dht.destroy()
-}
-
-function log (toString, obj) {
+function log (s, obj) {
   if (argv.json) {
     console.log(JSON.stringify(obj))
   } else {
-    console.log(toString(obj))
+    console.log(s)
   }
 }
 
 function resolveId (id, idFile) {
   if (id !== undefined) {
     // Skip writing the id to disc as passing in the id is supposed to increase the startup speed.
-    return { id: Buffer.from(id, 'hex'), method: 'argv' }
+    return Buffer.from(id, 'hex')
   }
-  // Allowing temporary files as relative paths but also supporting absolute paths.
-  idFile = path.resolve(os.tmpdir(), idFile)
-  let rndId
-  if (!fs.existsSync(idFile)) {
-    rndId = dhtrpc.id().slice(0, 32)
-    fs.writeFileSync(idFile, rndId)
-  }
-  id = fs.readFileSync(idFile).slice(0, 32)
-  return {
-    id,
-    method: rndId && rndId.equals(id) ? 'rnd' : 'fs',
-    store: idFile
-  }
+
+  if (!fs.existsSync(idFile)) fs.writeFileSync(idFile, dhtrpc.id().slice(0, 32))
+  return fs.readFileSync(idFile).slice(0, 32)
 }
